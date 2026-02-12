db05616abc40083e516ba420771442b4101cbee0
John Coleman <john.coleman@yale.edu>
2026-02-11 20:49:55 -0500

    Remove devispie2

--- devilspie2/.config/devilspie2/config.lua
+++
@@ -1,9 +0,0 @@
-debug_print("Application: " .. get_application_name())
-debug_print("Window: " .. get_window_name());
-if (get_application_name() == "KeePassXC") then
-  pin_window();
-end
--- Gnome settings
-if (get_application_name() == "gnome-control-center") then
-  pin_window();
-end
