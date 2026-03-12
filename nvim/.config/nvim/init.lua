949942ffbba9e7203fbeda346ff53373a57e1fdb
John Coleman <jcolemanl@odile>
2026-03-11 20:51:06 -0400

    Move orgfiles location and add WAITING keyword in nvim init.lua

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -45,9 +45,9 @@ lazy.setup({
 
 require('lualine').setup()
 require('orgmode').setup({
-      org_agenda_files = '~/orgfiles/**/*',
-      org_archive_location = '~/orgfiles/archives/%s_archive::',
-      org_default_notes_file = '~/orgfiles/refile.org',
+      org_agenda_files = '~/Documents/orgfiles/**/*',
+      org_archive_location = '~/Documents/orgfiles/archives/%s_archive::',
+      org_default_notes_file = '~/Documents/orgfiles/refile.org',
       org_todo_keywords = {'TODO','WAITING','|','DONE'},
 })
 
